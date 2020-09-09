82edd223bfcea62d17aaaae70f5b085b521984bb
Created unity Project <nrhymer@students.kennesaw.edu>
2020-09-09 18:07:16 -0400

    Complete push
    Todo list React app Example

---
+++ build/precache-manifest.efad1a064bcc4fc8ab2aacfae7b36153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147505f5e61c3fd8c39a3b6e8344208b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd6d0631c4aef806013a",
+    "url": "/static/css/main.bdacf6c2.chunk.css"
+  },
+  {
+    "revision": "4d9562894780d617d15b",
+    "url": "/static/js/2.cc226504.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cc226504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6d0631c4aef806013a",
+    "url": "/static/js/main.25ef76d1.chunk.js"
+  },
+  {
+    "revision": "f8ce95e23ae4eb434ece",
+    "url": "/static/js/runtime-main.ef835f46.js"
+  }
+]);
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the TodoList app v1.0.0. It is Part of a React Crash Course</p>
+        </React.Fragment>
+    )
+}
+
+
+export default About;
